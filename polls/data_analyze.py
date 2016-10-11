cfe156ccaf66cceefccd07562b72aed6da87498f
赵已绩 <zhaoyiji@hikvision.com.cn>
2016-10-11 19:56:33 +0800

    [ADD] [增加计算线段端点，起始2笔的方法]

--- polls/data_analyze.py
+++ polls/data_analyze.py
@@ -22,6 +22,8 @@ G = 1
 DD = 2
 GG = 3
 
+VAL = 1
+
 INCLUDE_STR = "include"
 UP = "up"
 DOWN = "down"
@@ -131,7 +133,7 @@ class HistoryData(object):
         return self._segment
 
     @classmethod
-    def _get_point_v2(cls, pen):
+    def _get_seg_point(cls, pen):
         """通过笔得到线段
 
         算法逻辑:
@@ -180,6 +182,115 @@ class HistoryData(object):
             pass
 
         return
+
+    @classmethod
+    def get_seg_init(cls, pen):
+        """ 线段端点计算初始化,得到标准特征序列的初始化分型的前2根，同时计算主方向，次方向。
+
+        :param pen:
+            pen[i][0] = index
+            pen[i][1] = val
+        :return: ["direct", [d1, g1, d2, g2, pos], [dd1, gg1, dd2, gg2, pos]]
+        """
+        if len(pen) < 5:
+            return []
+
+        major = []
+        minor = []
+        direct = "up"
+        if pen[0][VAL] > pen[1][VAL]:
+            direct = "down"
+
+        if direct == "up":
+            d1 = pen[0][VAL]
+            g1 = pen[1][VAL]
+            dd1 = pen[2][VAL]
+            gg1 = pen[1][VAL]
+            for i in range(3, len(pen)):
+                if i % 2 != 0:  # 次方向，计算趋势向下特征向量，特征向量方向为低到高,最终目标是得到底分型
+                    if len(minor) > 0:
+                        continue
+
+                    d = pen[i-1][VAL]
+                    g = pen[i][VAL]
+                    if d > d1:  # 分四种情况分析(1 VS 2，低点在前，高点在后)
+                        if g > g1:  # 高-高 趋势还在向上,前面一条可以忽略了
+                            d1 = d
+                            g1 = g
+                        else:  # 高-低,包含关系，前包后
+                            g1 = g
+                    else:
+                        if g >= g1:  # 低-高，包含关系,后包前
+                            d1 = d
+                        else:  # 低-低
+                            minor = [d1, g1, d, g, i]  # 得到符合要求的特征向量
+                            if len(major) > 0:
+                                break
+                else:  # 主方向，计算向上特征向量，特征向量方向为高到低，最终目标是得到顶分型
+                    if len(major) > 0:
+                        continue
+
+                    g = pen[i-1][VAL]
+                    d = pen[i][VAL]
+                    if d > dd1:  # 分四种情况分析(1 VS 2，低点在前，高点在后)
+                        if g > gg1:  # 高-高, 趋势还在向上，没意义忽略
+                            major = [dd1, gg1, d, g, i]  # 得到符合要求的特征向量
+                            if len(minor) > 0:
+                                break
+                        else:  # 高-低,包含关系，前包后
+                            dd1 = d
+                    else:
+                        if g >= gg1:  # 低-高，包含关系,后包前
+                            gg1 = g
+                        else:  # 低-低 趋势还在向下,前面一条可以忽略了
+                            dd1 = d
+                            gg1 = g
+        else:  # direct is "down"
+            d1 = pen[1][VAL]
+            g1 = pen[2][VAL]
+            dd1 = pen[1][VAL]
+            gg1 = pen[0][VAL]
+            for i in range(3, len(pen)):
+                if i % 2 != 0:  # 次方向，计算趋势向上特征向量，特征向量方向为高到低,最终目标是得到顶分型
+                    if len(minor) > 0:
+                        continue
+
+                    d = pen[i][VAL]
+                    g = pen[i-1][VAL]
+                    if d > d1:  # 分四种情况分析(1 VS 2，低点在前，高点在后)
+                        if g > g1:  # 高-高
+                            minor = [d1, g1, d, g, i]  # 得到符合要求的特征向量
+                            if len(major) > 0:
+                                break
+                        else:  # 高-低,包含关系，前包后
+                            d1 = d
+                    else:
+                        if g >= g1:  # 低-高，包含关系,后包前
+                            g1 = g
+                        else:  # 低-低 趋势还在向下,前面一条可以忽略了
+                            d1 = d
+                            g1 = g
+                else:  # 主方向，计算向下特征向量，特征向量方向为低到高，最终目标是得到底分型
+                    if len(major) > 0:
+                        continue
+
+                    d = pen[i-1][VAL]
+                    g = pen[i][VAL]
+                    if d > dd1:  # 分四种情况分析(1 VS 2，低点在前，高点在后)
+                        if g > gg1:  # 高-高, 趋势还在向上，没意义忽略
+                            dd1 = d
+                            gg1 = g
+                        else:  # 高-低,包含关系，前包后
+                            gg1 = g
+                    else:
+                        if g >= gg1:  # 低-高，包含关系,后包前
+                            dd1 = d
+                        else:  # 低-低 趋势还在向下,前面一条可以忽略了
+                            major = [dd1, gg1, d, g, i]  # 得到符合要求的特征向量
+                            if len(minor) > 0:
+                                break
+
+            return [direct, major, minor]
 
     @classmethod
     def get_point(cls, pen):
